63a264e0ddcf6f73b50534248df3365c4de47fb5
leeper <thosjleeper@gmail.com>
2014-08-07 17:26:29 +0200

    initialize search functions

--- R/search_docs.r
+++ R/search_docs.r
@@ -1,11 +1,75 @@
-search_docs <- function(){
+docs_search <- 
+function(query,
+         category = NULL,
+         language = NULL, # ISO 639-1 format
+         simple = TRUE,
+         num_start = 0,
+         num_results = 10,
+         ...
+){
+    a <- list()
+    if(!is.null(category))
+        a$category_id <- category
+    if(!is.null(language))
+        a$language <- language 
+    a$simple <- as.integer(simple)
+    stopifnot(num_start <= 1000)
+    a$num_start <- num_start
+    stopifnot(num_results <= 1000)
+    a$num_results <- num_results
+    scribd_query(method = "docs.search", 
+                 args = a,
+                 ...)
 }
 
-doc_categories <- function(){
+docs_categories <- 
+function(category = NULL,
+         subs = FALSE,
+         ...
+){
+    a <- list()
+    if(!is.null(category)) {
+        a$category_id <- category
+        if(!is.null(subs))
+            a$with_subcategories <- as.integer(subs)
+    }
+    scribd_query(method = "docs.getCategories", 
+                 args = a,
+                 ...)
 }
 
-featured_docs <- function(){
+docs_featured <- 
+function(scope = "new",
+         limit = NULL,
+         offset = NULL,
+         ...
+){
+    a <- list()
+    stopifnot(scope %in% c("new","hot"))
+    a$scope <- scope
+    if(!is.null(limit))
+        a$limit <- limit
+    if(!is.null(offset))
+        a$offset <- offset
+    scribd_query(method = "docs.featured", 
+                 args = a,
+                 ...)
 }
 
-browse_docs <- function(){
+docs_browser <- 
+function(sort = "popular",
+         limit = NULL,
+         offset = NULL,
+         ...
+){
+    a <- list()
+    stopifnot(sort %in% c("popular", "views", "newest"))
+    a$sort <- sort
+    if(!is.null(limit))
+        a$limit <- limit
+    if(!is.null(offset))
+        a$offset <- offset
+    scribd_query(method = "docs.browse", 
+                 args = a,
+                 ...)
 }
