15d081b0a04e5d83864ae28b4e20a30679d0c509
leeper <thosjleeper@gmail.com>
2014-08-07 17:27:02 +0200

    setup basic query function

--- R/http.r
+++ R/http.r
@@ -1,32 +1,54 @@
 scribd_query <-
 function(method, 
-         base_url = "http://api.scribd.com/api", 
+         args = NULL,
          verb = "GET",
          api_key = getOption("scribd_api_key"),
          session_key = getOption("scribd_session_key", NULL),
          my_user_id = getOption("scribd_user", NULL),
          secret_key = getOption("scribd_secret_key", NULL),
-         ...){
+         base_url = "https://api.scribd.com/api", 
+         ...)
+{
+    
+    args$method <- method
+    
+    # if no session_key but api_key, allow the operation anonymously
+    if(!is.null(session_key)) {
+        if(is.null(api_key))
+            stop("Must supply `api_key` and `session_key`")
+        args$session_key <- session_key
+    }
+    if(!is.null(api_key)){
+        args$api_key <- api_key
+    } else
+        stop("Must supply `api_key`")
+    
     # request signing
     if(!is.null(secret_key)){
         # alphabetically sort args by arg name (exclude `file` arg, if applicable)
-        a <- list()
+        a <- unlist(args)
+        if('file' %in% names(a))
+            a$file <- NULL
         # concatenate secret key and args without separators
-        s <- paste0(secret_key, names(a), a, collapse="")
+        s <- paste0(secret_key,
+                    names(a)[order(names(a))],
+                    a[order(names(a))], collapse="")
         # md5 encode
         d <- digest(s, algo="md5")
-        a <- append(a, c(api_sig = d))
+        args$api_sig <- d
     }
-    
-    # if no session_key, then pass a session-specific `my_user_id` variable (stored/retrieved from `options`)
-    # OR, if no session_key but api_key, allow the operation anonymously
     
     # execute request
     if(verb == "GET"){
-        out <- GET(paste0(base_url, '?method=', urlEscape(method)), ...)
+        out <- GET(base_url, query = args, ...)
     } else if(verb == "POST") {
-        out <- POST(paste0(base_url, '?method=', urlEscape(method)), ...)
+        out <- POST(base_url, query = args, ...)
     } else {
         stop("'verb' must be 'GET' or 'POST'")
-    }    
+    }
+    stop_for_status(out)
+    # perhaps this can be changed in the future when response is not `application/xml`
+    x <- xmlToList(cnt <- content(out, as = 'text'))
+    # need to parse errors here somehow
+    return(x)
 }
