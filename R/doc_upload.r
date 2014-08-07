03b450149afc688960ad22d8a87c6b34099af478
leeper <thosjleeper@gmail.com>
2014-08-07 17:44:51 +0200

    initialize doc functions

--- R/doc_upload.r
+++ R/doc_upload.r
@@ -1,6 +1,76 @@
-upload_doc <- function(){
-# also handle docs.uploadFromUrl
+.filetypes <- c("pdf", "txt", "ps", "rtf", "epub", "key", "odt", 
+                "odp", "ods", "odg", "odf", "sxw", "sxc", "sxi", 
+                "sxd", "doc", "ppt", "pps", "xls", "docx", "pptx", 
+                "ppsx", "xlsx")
+
+doc_upload <-
+function(file = NULL,
+         url = NULL,
+         access = "public",
+         doc = NULL,
+         paid_content = FALSE,
+         drm = NULL,
+         wait = FALSE,
+         ...
+){
+    a <- list()
+    if(!is.null(access)) {
+        stopifnot(access %in% c("public","private"))
+        a$access <- access
+    }
+    if(!is.null(doc)){
+        a$rev_id <- doc
+    }
+    a$paid_content <- as.integer(paid_content)
+    if(paid_content){
+        if(!is.null(drm)){
+            stopifnot(drm %in% c("download-pdf","download-pdf-orig","view-only"))
+            a$download_and_drm <- drm
+        }
+    }
+    
+    if(!is.null(file)){
+        stopifnot(file_ext(file) %in% .filetypes)
+        out <-
+        scribd_query(method = "docs.upload", 
+                     args = a,
+                     verb = "POST",
+                     body = list(file = upload_file(file)),
+                     ...)
+    } else if(!is.null(url)){
+        a$url <- URLencode(URLencode(url), reserved = TRUE)
+        out <-
+        scribd_query(method = "docs.uploadFromUrl", 
+                     args = a, ...)
+    } else {
+        stop("Must specify `file` or `url`")
+    }
+    if(wait){
+        s <- "PROCESSSING"
+        while(s == "PROCESSING") {
+            s <- conversion_status(out$doc_id)
+            Sys.sleep(1)
+        }
+        if(s %in% c("DISPLAYABLE","DONE"))
+            message("Document is ", tolower(s))
+        else if(s == "")
+            warning("Document ", out$doc_id, " conversion failed!")
+    }
+    return(out)
 }
 
-upload_thumbnail <- function(){
+upload_thumbnail <- function(file, doc, ...){
+    a <- list(doc_id = doc)
+    scribd_query(method = "docs.getConversionStatus", 
+                 args = a,
+                 verb = "POST",
+                 body = list(file = upload_file(file)),
+                 ...)
+}
+
+conversion_status <- function(doc, ...){
+    a <- list(doc_id = doc)
+    scribd_query(method = "docs.getConversionStatus", 
+                 args = a,
+                 ...)
 }
---
+++ R/docs.r
@@ -0,0 +1,160 @@
+doc_list <-
+function(limit = NULL,
+         offset = NULL,
+         ...
+){
+    a <- list()
+    if(!is.null(limit))
+        a$limit <- limit
+    if(!is.null(offset))
+        a$offset <- offset
+    scribd_query(method = "docs.getList", 
+                 args = a,
+                 ...)
+}
+
+doc <- 
+function(doc, 
+         fmt = "original",
+         url_only = TRUE,
+         ...
+){
+    a <- list()
+    a$doc_id <- doc
+    stopifnot(fmt %in% c("pdf","txt","original"))
+    a$doc_type <- fmt
+    out <- 
+    scribd_query(method = "docs.getDownloadUrl", 
+                 args = a,
+                 ...)
+    if(!url_only) {
+        if(!is.null(out$download_link[[1]])) {
+            f <- GET(out$download_link[[1]])
+            if(fmt == "txt")
+                return(content(f, as="text"))
+            else
+                return(content(f, as="raw"))
+        } else {
+            stop("Download link is NULL")
+        }
+    } else
+        return(out)
+}
+
+doc_thumbnail <-
+function(doc,
+         width = NULL,
+         height = NULL,
+         url_only = TRUE,
+         ...){
+    a <- list()
+    a$doc_id <- doc
+    if(!is.null(width))
+        a$width <- width
+    if(!is.null(height))
+        a$heigh <- height
+    out <- 
+    scribd_query(method = "thumbnail.get", 
+                 args = a,
+                 ...)
+    if(!url_only) {
+        if(!is.null(out$thumbnail_url[[1]])) {
+            f <- GET(out$thumbnail_url[[1]])
+            return(content(f, as = "raw"))
+        } else {
+            stop("Download link is NULL")
+        }
+    } else
+        return(out)
+}
+
+doc_stats <- 
+function(doc, ...){
+    a <- list()
+    a$doc_id <- doc
+    scribd_query(method = "docs.getStats", 
+                 args = a,
+                 ...)
+}
+
+doc_settings <-
+function(doc, ...){
+    a <- list()
+    a$doc_id <- doc
+    scribd_query(method = "docs.getSettings", 
+                 args = a,
+                 ...)
+}
+
+doc_change <- 
+function(doc,
+         isbn = NULL,
+         title = NULL,
+         description = NULL,
+         access = NULL,
+         license = NULL,
+         category = NULL,
+         tags = NULL,
+         fmt = NULL,
+         author = NULL,
+         publisher = NULL,
+         pub_date = NULL,
+         edition = NULL,
+         disable_select_text = NULL,
+         disable_related = NULL,
+         ...
+){
+    a <- list()
+    a$doc_ids <- paste(doc, collapse=",")
+    if(!is.null(isbn))
+        a$isbn <- isbn
+    if(!is.null(title))
+        a$title <- title
+    if(!is.null(description))
+        a$description <- description
+    if(!is.null(access)) {
+        stopifnot(access %in% c("public", "private"))
+        a$access <- access
+    }
+    if(!is.null(license)) {
+        stopifnot(license %in% c("by", "by-nc", "by-nc-nd", 
+                                 "by-nc-sa", "by-nd", "by-sa", 
+                                 "c", "pd"))
+        a$license <- license
+    }
+    if(!is.null(category))
+        a$category_id <- category
+    if(!is.null(tags))
+        a$tags <- paste(tags, collapse=",")
+    if(!is.null(fmt)) {
+        stopifnot(any(!fmt %in% c("pdf", "text", "original")))
+        a$download_formats <- paste(fmt, collapse=",")
+    }
+    if(!is.null(author))
+        a$author <- author
+    if(!is.null(publisher))
+        a$publisher <- publisher
+    if(!is.null(pub_date))
+        a$when_published <- pub_date
+    if(!is.null(edition))
+        a$edition <- edition
+    if(!is.null(disable_select_text))
+        a$disable_select_text <- as.integer(disable_select_text)
+    if(!is.null(disable_related))
+        a$disable_related_docs <- as.integer(disable_related)
+    
+    scribd_query(method = "docs.changeSettings", 
+                 args = a,
+                 ...)
+
+}
+
+doc_delete <- 
+function(doc, ...){
+    a <- list()
+    a$doc_id <- doc
+    scribd_query(method = "docs.delete", 
+                 args = a,
+                 ...)
+
+}
